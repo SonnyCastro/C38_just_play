beaeb66ce3bd6dd45c4f3ea8eb79526fb77b57a7
Sonny Castro <sonnycastro0606@gmail.com>
2020-08-25 18:31:30 -0400

    Connected Login and Register with db and routes

---
+++ client/src/pages/Signup.jsx
@@ -0,0 +1,79 @@
+import React, { useState, useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Form, Button } from 'react-bootstrap';
+import { AppContext } from '../context/AppContext';
+import axios from 'axios';
+import swal from 'sweetalert';
+
+const SignUp = ({ history }) => {
+  const [formData, setFormData] = useState(null);
+  const { setCurrentUser } = useContext(AppContext);
+
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post('/api/users/', formData)
+      .then((response) => {
+        sessionStorage.setItem('user', response.data);
+        setCurrentUser(response.data);
+        history.push('/');
+      })
+      .catch((error) => swal('Error', 'Please check the inputs', 'warning'));
+  };
+
+  return (
+    <Container className="container d-flex flex-column align-items-center justify-content-center fullscreen">
+      <h1 className="mb-4">Create Account!</h1>
+      <Form style={{ width: 300 }} onSubmit={handleSubmit}>
+        <Form.Group>
+          <Form.Label htmlFor="fullName">Full Name</Form.Label>
+          <Form.Control
+            id="fullName"
+            type="text"
+            placeholder="Full Name"
+            name="name"
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label htmlFor="email">Email Address</Form.Label>
+          <Form.Control
+            id="email"
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label htmlFor="password">Password</Form.Label>
+          <Form.Control
+            id="password"
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Form.Group
+          controlId="formBasicCheckbox"
+          className="d-flex justify-content-end"
+        >
+          <Form.Check type="checkbox" label="Check admin to make events" />
+        </Form.Group>
+        <Form.Group className="d-flex justify-content-center">
+          <Button type="submit">Create Account</Button>
+        </Form.Group>
+      </Form>
+      <Link className="mt-4" to="/login">
+        Already have an account? Login.
+      </Link>
+    </Container>
+  );
+};
+
+export default SignUp;
